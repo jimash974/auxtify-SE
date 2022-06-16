ebf3b42adbe2975dc77b69689221fac81f852d2d
M4ruu <putu.yudananta@binus.ac.id>
2022-06-16 22:25:55 +0700

    accountsett

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -2,6 +2,27 @@
 @section('contentFill')
 <link rel="stylesheet" href="/css/account.css">
 
-
+<div class="account-content">
+    <div class="top-bg"></div>
+    <div class="main-content">
+        <img src="/images/user_profile.png" alt="">
+        <div class="text-content">
+            <h1 class="fw-bold mb-0">SUSI SUSANTI</h1>
+            <p class="fs-4 mb-0">Jakarta,Indonesia</p>
+            <p class="fw-bold">User</p>
+            <div class="following-followers">
+                <div class="followers fs-4">
+                    <h1>157</h1>
+                    <h2>Followers</h2>
+                </div>
+                <div class="vr"></div>
+                <div class="following fs-4">
+                    <h1>92</h1>
+                    <h2>Following</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
